e05f41123a2d9a5a4c05187aad11eb9ac4acd348
AleBombi <118299985+AleBombi@users.noreply.github.com>
2023-04-01 23:50:45 +0200

    gioco finito

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import random
 import math
 import pygame
@@ -8,79 +8,213 @@ pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-WIDTH, HEIGHT = 1000, 8000
+WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def flip(sprites):
-        return [pygame.transform.flip(sprite, True, False) for sprite in sprites] #indicano come flippare l'immagine
 
-def load_sprite_sheets(dir1, dir2, width, height, direction=False): #in questo modo direction è di default settato a False
+def flip(sprites):
+    # indicano come flippare l'immagine
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+# in questo modo direction è di default settato a False
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
     path = join("assets", dir1, dir2)
-    images = [f for f in listdir(path) if isfile(join(path, f))] #carica ogni file presente il path se path è valido
+    # carica ogni file presente il path se path è valido
+    images = [f for f in listdir(path) if isfile(join(path, f))]
     # os.path.isfile() method in Python is used to check whether the specified path is an existing regular file or not
 
     all_sprites = {}
 
     for image in images:
-        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha() #convert alpha permette di avere lo sfondo trasparente
+        # convert alpha permette di avere lo sfondo trasparente
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
 
         sprites = []
-        for i in range(sprite_sheet.get_width()//width): # width larghezza dell'intera immagine
-            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32) #stiamo creando la superficie di un'immagine singola
+        # width larghezza dell'intera immagine
+        for i in range(sprite_sheet.get_width()//width):
+            # stiamo creando la superficie di un'immagine singola
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
             rect = pygame.Rect(i*width, 0, width, height)
-            surface.blit(sprite_sheet, (0,0), rect) 
+            surface.blit(sprite_sheet, (0, 0), rect)
             sprites.append(pygame.transform.scale2x(surface))
 
-        if direction: # siccome le immagini hanno come direzione destra, in una animazione multidirezionale dobbiamo flippare quelle immagini per avere la direzione a sinistra
+        if direction:  # siccome le immagini hanno come direzione destra, in una animazione multidirezionale dobbiamo flippare quelle immagini per avere la direzione a sinistra
             all_sprites[image.replace(".png", "")+"_right"] = sprites
             all_sprites[image.replace(".png", "")+"_left"] = flip(sprites)
         else:
             all_sprites[image.replace(".png", "")] = sprites
-    
+
     return all_sprites
 
-class Player(pygame.sprite.Sprite): # modo per eridare le proprietà di Sprite
-    COLOR=(255, 0, 0)
-    GRAVITY = 1
 
+def get_block(size):  # size è la grandezza del blocco che vogliamo
+    path = join("assets", "Terrain", "Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    # lo usiamo per indicare quale immagine del terreno vogliamo usare nell'immgagine Terrain.png (96,0 sono le coordianate del punto in alto a sx di dove inizia l'immagine simil-terra)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
+    return pygame.transform.scale2x(surface)
+
+
+class Player(pygame.sprite.Sprite):  # modo per eridare le proprietà di Sprite
+    COLOR = (255, 0, 0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
+    ANIMATION_DELAY = 2
 
     def __init__(self, x, y, width, height):
-        self.rect=pygame.Rect(x,y, width, height) # self rappresenta l'instanza della classe, permette di accedere agli attributi e metodi della classe
-        self.x_vel=0
-        self.y_vel=0
-        self.mask=None
-        self.direction="left" 
-        self.animation_count=0
-        self.fall_count=0
-    
+        super().__init__()
+        # self rappresenta l'instanza della classe, permette di accedere agli attributi e metodi della classe
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+        self.jump_count = 0
+        self.hit = False
+        self.hit_count = 0
+
+    def jump(self):
+        self.y_vel = -self.GRAVITY*8
+        self.animation_count = 0
+        self.jump_count += 1
+        if self.jump_count == 1:
+            self.fall_count = 0
+
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
 
+    def make_hit(self):
+        self.hit = True
+        self.hit_count = 0
+
     def move_left(self, vel):
-        self.x_vel = -vel # velocità negativa perchè dobbiamo sottrarre alle ascisse
+        self.x_vel = -vel  # velocità negativa perchè dobbiamo sottrarre alle ascisse
         if self.direction != "left":
             self.direction = "left"
-            self.animation_count= 0
+            self.animation_count = 0
 
     def move_right(self, vel):
-        self.x_vel = vel   
+        self.x_vel = vel
         if self.direction != "right":
             self.direction = "right"
-            self.animation_count= 0
+            self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel+= min(1, (self.fall_count/fps)*self.GRAVITY)
+        self.y_vel += min(1, (self.fall_count/fps)*self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
+        if self.hit:
+            self.hit_count += 1
+        if self.hit_count > fps * 2:
+            self.hit = False
+            self.hit_count = 0
+
         self.fall_count += 1
+        self.update_sprite()
+
+    def landed(self):
+        self.fall_count = 0  # così smettiamo di aggiungere gravità
+        self.y_vel = 0
+        self.jump_count = 0
+
+    def hit_head(self):
+        self.count = 0
+        self.y_vel *= -1
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.hit:
+            sprite_sheet = "hit"
+        elif self.y_vel < 0:
+            if self.jump_count == 1:
+                sprite_sheet = "jump"
+            elif self.jump_count == 2:
+                sprite_sheet = "double_jump"
+        elif self.y_vel > self.GRAVITY*2:
+            sprite_sheet = "fall"
+        elif self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        # sprite_index indica quale immagine stiamo visualizzando rispetto ad una collezione di animazione (ad esempio in hit)
+        # ogni ANIMATION_DELAY mostriamo una sprite diversa (ovvero un'immgaine diversa)
+        sprite_index = (self.animation_count //
+                        self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+        self.update()
+
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        # permette di ridimensionare il rettangolo che usiamo per il personaggio in base alla sprite che stiamo utilizzando
+        # mapping dei pixel sulle sprite, permette di ottenere collisioni pixel perfect
+        self.mask = pygame.mask.from_surface(self.sprite)
+
+    def draw(self, win, offset_x):
+        win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
 
 
-    def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+class Object(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, width, height, name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
+
+    def draw(self, win, offset_x):
+        win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
+
+
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = get_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+class Fire(Object):
+    ANIMATION_DELAY = 3
+
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height, "fire")
+        self.fire = load_sprite_sheets("Traps", "Fire", width, height)
+        self.image = self.fire["off"][0]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.animation_count = 0
+        self.animation_name = "off"
+
+    def on(self):
+        self.animation_name = "on"
+
+    def off(self):
+        self.animation_name = "off"
+
+    def loop(self):
+        sprites = self.fire[self.animation_name]
+        sprite_index = (self.animation_count //
+                        self.ANIMATION_DELAY) % len(sprites)
+        self.image = sprites[sprite_index]
+        self.animation_count += 1
+
+        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.image)
+
+        if self.animation_count//self.ANIMATION_DELAY > len(sprites):
+            self.animation_count = 0
 
 
 def get_background(name):
@@ -88,37 +222,96 @@ def get_background(name):
     _, _, width, height = image.get_rect()
     tiles = []
     for i in range(WIDTH // width + 1):
-        for j in range(HEIGHT// height + 1):
-            pos = (i*width, j*height) # tupla che contiene le posizioni
+        for j in range(HEIGHT // height + 1):
+            pos = (i*width, j*height)  # tupla che contiene le posizioni
             tiles.append(pos)
 
     return tiles, image
 
 
-
-def draw(window, background, bg_image, player):
+def draw(window, background, bg_image, player, objects, offset_x):
     for tile in background:
         window.blit(bg_image, tile)
-    
-    player.draw(window)
-   
+
+    for obj in objects:
+        obj.draw(window, offset_x)
+
+    player.draw(window, offset_x)
+
     pygame.display.update()
 
 
-def handle_move(player):
+def handle_vertical_collision(player, objects, dy):
+    collided_objects = []
+    for obj in objects:
+        # ci dice se il player collide con l'oggetto
+        if pygame.sprite.collide_mask(player, obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top  # metti il player sopra l'oggetto
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head()
+
+            collided_objects.append(obj)
+
+    return collided_objects
+
+
+def collide(player, objects, dx):
+    player.move(dx, 0)
+    player.update()
+    collideed_object = None
+    for obj in objects:
+        # controllo se il personaggio colliderà con un oggetto in quella direzione
+        if pygame.sprite.collide_mask(player, obj):
+            collideed_object = obj
+            break
+
+    player.move(-dx, 0)  # devo fare il movimento inverso
+    player.update()
+    return collideed_object
+
+
+def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
-    player.x_vel=0 #necessario affinche il personaggio si fermi quando smettiamo di premere un tasto
-    if keys[pygame.K_LEFT]: # per utilizzare il tasto a per andare a sinistra si deve sostituire K_LEFT con K_a
+    # necessario affinche il personaggio si fermi quando smettiamo di premere un tasto
+    player.x_vel = 0
+    collide_left = collide(player, objects, -PLAYER_VEL*2)
+    collide_right = collide(player, objects, PLAYER_VEL*2)
+
+    # per utilizzare il tasto a per andare a sinistra si deve sostituire K_LEFT con K_a
+    # controlliamo se possimao muoverci a sinistra o se avremmo una collisione
+    if keys[pygame.K_LEFT] and not collide_left:
         player.move_left(PLAYER_VEL)
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and not collide_right:
         player.move_right(PLAYER_VEL)
+
+    vertical_collide = handle_vertical_collision(player, objects, player.y_vel)
+    to_check = [collide_left, collide_right, *vertical_collide]
+    for obj in to_check:
+        if obj and obj.name == "fire":
+            player.make_hit()
+
 
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Gray.png")
 
-    player = Player(100,100,50,50)
+    block_size = 96
+
+    player = Player(100, 100, 50, 50)
+    fire = Fire(100, HEIGHT-block_size - 64, 16, 32)
+    fire.on()
+    # blocchi a sinistra e destra
+    floor = [Block(i*block_size, HEIGHT-block_size, block_size)
+             for i in range(-WIDTH//block_size, WIDTH*2 // block_size)]
+
+    objects = [*floor, Block(0, HEIGHT-block_size*2, block_size),
+               Block(block_size*3, HEIGHT-block_size*4, block_size), fire]
+    offset_x = 0
+    scroll_area_width = 200
 
     run = True
     while run:
@@ -129,17 +322,22 @@ def main(window):
                 run = False
                 break
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player.jump_count < 2:
+                    player.jump()
+
         player.loop(FPS)
-        handle_move(player)
-        draw(window, background, bg_image, player)
+        fire.loop()
+        handle_move(player, objects)
+        draw(window, background, bg_image, player, objects, offset_x)
+
+        if ((player.rect.right-offset_x >= WIDTH - scroll_area_width and player.x_vel > 0) or (player.rect.left - offset_x <= scroll_area_width and player.x_vel < 0)):
+            # quando il personaggio è a scroll_area_width pixel dal bordo allora inizia a spostare la "telecamera"
+            offset_x += player.x_vel
 
     pygame.quit()
     quit()
-    
-   
-
 
 
 if __name__ == "__main__":
     main(window)
-
